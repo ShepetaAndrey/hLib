8ce2ecda5beb0ae479df70ae07e7e7cd96793eab
AndreyShepeta <andrey.shepeta@gmail.com>
2020-04-19 20:11:17 +0300

    fix vuex import

--- store/index.js
+++ store/index.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import Vuex from 'Vuex';
+import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
@@ -29,9 +29,7 @@ const store = () => new Vuex.Store({
     }
   },
   actions: {
-    getBooks: function () {
-
-    }
+    getBooks: function () {}
   }
 })
 
