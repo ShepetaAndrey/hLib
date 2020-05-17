ecba7aefb8cadafa099aa6b80c2461bdb2f52c13
AndreyShepeta <andrey.shepeta@gmail.com>
2020-05-17 21:52:40 +0300

    [route]: add test book page

---
+++ pages/book/_id.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-content>
+    <v-container>
+      <pre>{{ book }}</pre>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      bookId: this.$route.params.id,
+      book: {}
+    };
+  },
+  async created() {
+    this.book = await this.$books.getBook(this.bookId);
+  }
+};
+</script>
+
+<style>
+</style>
