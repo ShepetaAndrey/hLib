ca5a6bd387bbb9bcedcc7c4883b2610bef9486fd
AndreyShepeta <andrey.shepeta@gmail.com>
2020-05-17 23:12:37 +0300

    [serach/api]: minor code style fix

--- plugins/booksApi/google-books-api.js
+++ plugins/booksApi/google-books-api.js
@@ -35,9 +35,8 @@ export function getCover(book, zoom=1) {
     img: 1, // send picture or not
     zoom: zoom //less value - less picture
   }
-  const param = (key, value) => `${key}=${value}`; 
   const params = Object.entries(q)
-    .map(([key, value]) => param(key, value))
+    .map(([key, value]) => `${key}=${value}`)
     .join('&');
   return path + params;
 }
