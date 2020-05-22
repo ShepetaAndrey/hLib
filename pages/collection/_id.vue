d8242d4c4a429ac489dd6f5316224893a88398bf
AndreyShepeta <andrey.shepeta@gmail.com>
2020-05-22 21:38:30 +0300

    [router]: add collection page and collection id page

---
+++ pages/collection/_id.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-content>
+    <v-container>
+      <div>this is: {{ id }}</div>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: this.$route.params.id
+    };
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ pages/collection/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-content>
+    <v-container>
+      <div>{{ url }}</div>
+      <v-list v-for="c in collections" :key="c">
+        <v-list-item>
+          <nuxt-link :to="collectionLink(c)">{{ c }}</nuxt-link>
+        </v-list-item>
+      </v-list>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      url: this.$route.name,
+      collections: [
+        'junior collection',
+        'middle collection',
+        'senior collection'
+      ]
+    };
+  },
+  methods: {
+    collectionLink(id) {
+      return this.url + '/' + id;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
