a7bd4e96e8be74a298fef7cdbae21ad93d78ad3a
AndreyShepeta <andrey.shepeta@gmail.com>
2020-05-23 20:30:26 +0300

    [search]: add trigger points for search items style

--- components/SearchListItem.vue
+++ components/SearchListItem.vue
@@ -1,10 +1,10 @@
 <template>
   <v-row justify="center" align="center" class="mx-auto d-flex flex-column">
     <v-card width="100%" class="mb-1 d-flex flex-row" outlined>
-      <v-col class="col-md-3" lg3>
-        <v-img :src="imgSrc" class="mx-auto"></v-img>
+      <v-col class="col-md-3 col-sm-2 col-3" lg3>
+        <v-img :src="imgSrc" aspect-ratio="0.9" max-width="80px"></v-img>
       </v-col>
-      <v-col class="col-md-9" lg9>
+      <v-col class="col-md-9 col-sm-10 col-9" lg9>
         <p>{{ title }}</p>
         <p style="overflow: hide;">
           <i>{{ author }}</i>
