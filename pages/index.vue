78eab7eab306450236f11c90d95f5229c831b3f1
AndreyShepeta <andrey.shepeta@gmail.com>
2020-05-23 21:56:42 +0300

    [pages]: remove boilerplate text from home page

--- pages/index.vue
+++ pages/index.vue
@@ -78,11 +78,6 @@
           </v-carousel-item>
         </v-carousel>
       </v-row>
-      <!-- some text content -->
-      <v-row justify="center">
-        <v-col lg6>{{ dataBoilerpalte }}</v-col>
-        <v-col lg6>{{ dataBoilerpalte }}</v-col>
-      </v-row>
     </v-container>
   </v-content>
 </template>
@@ -90,10 +85,7 @@
 <script>
 export default {
   data() {
-    return {
-      dataBoilerpalte:
-        'Morbi mattis ullamcorper velit. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Fusce convallis metus id felis luctus adipiscing. Aenean massa. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nulla consequat massa quis enim. Praesent venenatis metus at tortor pulvinar varius. Donec venenatis vulputate lorem. Phasellus accumsan cursus velit. Pellentesque ut neque.'
-    };
+    return {};
   }
 };
 </script>
