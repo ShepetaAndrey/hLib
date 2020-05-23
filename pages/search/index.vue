82b19b10bafd45756452a45f8f4ce5fc375edc18
AndreyShepeta <andrey.shepeta@gmail.com>
2020-05-23 20:31:33 +0300

    [search]: update for mobile versions

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -1,12 +1,12 @@
 <template>
   <v-content light class="white">
-    <v-row justify="center" align="center" class="mx-auto pa-6" style="max-width: 600px;">
+    <v-row justify="center" align="center" class="mx-auto pa-6">
       <v-icon size="4em">mdi-magnify</v-icon>
       <span class="display-3">Search for a book</span>
     </v-row>
     <v-row justify="center">
       <v-col style="max-width: 650px;">
-        <search-input />
+        <search-input stand-alone-page :srollable="false" />
       </v-col>
     </v-row>
   </v-content>
