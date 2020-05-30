c1fb8bc83f2264fd05300cc885134cc11e45f3ca
AndreyShepeta <andrey.shepeta@gmail.com>
2020-05-30 22:42:15 +0300

    [pages]: add collection index and id pages

--- pages/collection/_id.vue
+++ pages/collection/_id.vue
@@ -1,7 +1,32 @@
 <template>
   <v-content>
     <v-container>
-      <div>this is: {{ id }}</div>
+      <v-flex class="text-center display-1 justify-center">{{ collectionId }}</v-flex>
+      <v-divider class="my-5"></v-divider>
+      <v-flex justify-center row>
+        <v-btn nuxt to="/search" class="d-flex my-3">Add book</v-btn>
+        <v-switch class="d-flex ml-3" v-model="removeProcess" light label="Remove mode"></v-switch>
+      </v-flex>
+      <v-divider class="my-5"></v-divider>
+      <v-flex row>
+        <div class="col-6 col-sm-3 col-md-2 col-lg-2" v-for="id in listOfBookIds" :key="id">
+          <nuxt-link :to="bookLink(id)">
+            <v-card hover class="py-3">
+              <v-img height="150px" contain :src="$books.getCover({ id: id })"></v-img>
+              <v-btn
+                v-if="removeProcess"
+                @click.prevent="removeBookFromCollection(id)"
+                rounded
+                small
+                fab
+                class="red delete-btn"
+              >
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+            </v-card>
+          </nuxt-link>
+        </div>
+      </v-flex>
     </v-container>
   </v-content>
 </template>
@@ -10,11 +35,48 @@
 export default {
   data() {
     return {
-      id: this.$route.params.id
+      collectionId: this.$route.params.id,
+      books: [],
+      removeProcess: false
     };
+  },
+  computed: {
+    listOfBookIds() {
+      const data = this.$store.getters['collection/getById'](this.collectionId);
+      if (typeof data !== 'undefined') {
+        return this.$store.getters['collection/getById'](this.collectionId)
+          .books;
+      } else {
+        return [];
+      }
+    }
+  },
+  mounted() {
+    this.$store.commit('collection/initState', {
+      collections: localStorage.getItem('collections')
+    });
+  },
+  methods: {
+    async getBookInfo(bookId) {
+      return await this.$books.getBook(bookId);
+    },
+    bookLink(id) {
+      return '/book/' + id;
+    },
+    removeBookFromCollection(bookId) {
+      this.$store.commit('collection/deleteBook', {
+        bookId: bookId,
+        collectionId: this.collectionId
+      });
+    }
   }
 };
 </script>
 
 <style>
+.delete-btn {
+  position: absolute;
+  top: -10px;
+  right: -10px;
+}
 </style>
--- pages/collection/index.vue
+++ pages/collection/index.vue
@@ -1,31 +1,126 @@
 <template>
   <v-content>
-    <v-layout>
-      <div>{{ url }}</div>
-      <v-list v-for="c in collections" :key="c">
-        <v-list-item>
-          <nuxt-link :to="collectionLink(c)">{{ c }}</nuxt-link>
-        </v-list-item>
-      </v-list>
-    </v-layout>
+    <v-container>
+      <p class="display-3 regular text-center">Collections</p>
+      <v-flex row justify-center class="ma-0">
+        <div class="d-flex col-12 col-sm-6 col-md-4 justify-center">
+          <v-dialog v-model="dialogCreate" scrollable max-width="300px">
+            <template v-slot:activator="{ on }">
+              <v-btn v-on="on">
+                <v-icon left>mdi-plus</v-icon>Create Collection
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title>Create Collection</v-card-title>
+              <v-divider></v-divider>
+              <v-text-field
+                class="pa-3"
+                placeholder="Collection name"
+                hint="Write name of new collection"
+                outlined
+                v-model="collectionName"
+              />
+              <v-card-actions>
+                <v-btn color="brown darken-1" text @click="dialogCreate = false">Close</v-btn>
+                <v-btn
+                  color="brown darken-1"
+                  :disabled="!collectionName"
+                  text
+                  @click="createCollection()"
+                >Create</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </div>
+        <div class="d-flex col-12 col-sm-6 col-md-4 justify-center">
+          <v-dialog v-model="dialogDelete" scrollable max-width="300px">
+            <template v-slot:activator="{ on }">
+              <v-btn v-on="on">
+                <v-icon left>mdi-delete</v-icon>Delete Collection
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title>Delete Collection</v-card-title>
+              <v-divider></v-divider>
+              <v-card-text class="d-flex" style="height: 300px;">
+                <v-radio-group v-if="collectionIdList.length" v-model="dialogRemoveColModel" column>
+                  <v-radio
+                    :label="colId"
+                    :value="colId"
+                    v-for="colId in collectionIdList"
+                    :key="colId"
+                  ></v-radio>
+                </v-radio-group>
+                <div v-else class="d-flex display-2 align-center text-center">nothing to remove :(</div>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn color="brown darken-1" text @click="dialogDelete = false">Close</v-btn>
+                <v-btn
+                  color="brown darken-1"
+                  :disabled="!dialogRemoveColModel"
+                  text
+                  @click="deleteCollection()"
+                >Delete</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </div>
+      </v-flex>
+      <hlib-collection-list :collectionIdList="collectionIdList" />
+    </v-container>
   </v-content>
 </template>
 
 <script>
+import CollectionListVue from '../../components/collection/CollectionList.vue';
+
 export default {
+  components: {
+    'hlib-collection-list': CollectionListVue
+  },
   data() {
     return {
       url: this.$route.name,
-      collections: [
-        'junior collection',
-        'middle collection',
-        'senior collection'
-      ]
+      collectionName: '',
+      dialogCreate: false,
+      dialogDelete: false,
+      dialogRemoveColModel: ''
     };
+  },
+  computed: {
+    collectionIdList() {
+      return this.$store.getters['collection/getAll'].map(col => col.id);
+    }
   },
   methods: {
     collectionLink(id) {
       return this.url + '/' + id;
+    },
+    createCollection() {
+      if (this.collectionName) {
+        this.$store.commit('collection/addCollection', {
+          collectionId: this.collectionName
+        });
+        localStorage.setItem(
+          'collections',
+          JSON.stringify(this.$store.getters['collection/getAll'])
+        );
+      }
+      this.collectionName = '';
+      this.dialogCreate = false;
+    },
+    deleteCollection() {
+      if (this.dialogRemoveColModel.length) {
+        this.$store.commit('collection/deleteCollection', {
+          collectionId: this.dialogRemoveColModel
+        });
+        // localStorage.setItem(
+        //   'collections',
+        //   JSON.stringify(this.$store.getters['collection/getAll'])
+        // );
+      }
+      this.dialogRemoveColModel = '';
+      this.dialogDelete = false;
     }
   }
 };
